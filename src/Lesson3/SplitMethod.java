3d124329b96b91f60b71efda517ff2611d3da332
Victoriiiia <viktoriyagolovataya@gmail.com>
2023-10-17 20:48:15 +0900

    second branch

---
+++ src/Lesson3/SplitMethod.java
@@ -0,0 +1,86 @@
+package Lesson3;
+
+import java.util.Locale;
+
+public class SplitMethod {
+    public static void main(String[] args) {
+        String st = "Completely random text in English. " +
+                "In it, we just need to determine how many" +
+                " times the character 'a' occurs there." +
+                " And we can use the split method and the length method.";
+        String buk;
+        System.out.println(st.length());
+        String[] arrayOfWords = st.split("\\s");
+        String word0 = arrayOfWords [0];
+        String word1 = arrayOfWords [1];
+        String word2 = arrayOfWords [2];
+        String word3 = arrayOfWords [3];
+        String word4= arrayOfWords [4];
+        String word5 = arrayOfWords [5];
+        String word6 = arrayOfWords [6];
+        String word7 = arrayOfWords [7];
+        String word8 = arrayOfWords [8];
+        String word9= arrayOfWords [9];
+        String word10= arrayOfWords [10];
+        String word11= arrayOfWords [11];
+        String word12= arrayOfWords [12];
+        String word13= arrayOfWords [13];
+        String word14= arrayOfWords [14];
+        String word15= arrayOfWords [15];
+        String word16= arrayOfWords [16];
+        String word17= arrayOfWords [17];
+        String word18= arrayOfWords [18];
+        String word19= arrayOfWords [19];
+        String word20= arrayOfWords [20];
+        String word21= arrayOfWords [21];
+        String word22= arrayOfWords [22];
+        String word23= arrayOfWords [23];
+        String word24= arrayOfWords [24];
+        String word25= arrayOfWords [25];
+        String word26= arrayOfWords [26];
+        String word27= arrayOfWords [27];
+        String word28= arrayOfWords [28];
+        String word29= arrayOfWords [29];
+        String word30= arrayOfWords [30];
+        System.out.println(word0);
+        System.out.println(word1);
+        System.out.println(word2);
+        System.out.println(word3);
+        System.out.println(word4);
+        System.out.println(word5);
+        System.out.println(word6);
+        System.out.println(word7);
+        System.out.println(word8);
+        System.out.println(word9);
+        System.out.println(word10);
+        System.out.println(word11);
+        System.out.println(word12);
+        System.out.println(word13);
+        System.out.println(word14);
+        System.out.println(word15);
+        System.out.println(word16);
+        System.out.println(word17);
+        System.out.println(word18);
+        System.out.println(word19);
+        System.out.println(word20);
+        System.out.println(word21);
+        System.out.println(word22);
+        System.out.println(word23);
+        System.out.println(word24);
+        System.out.println(word25);
+        System.out.println(word26);
+        System.out.println(word27);
+        System.out.println(word28);
+        System.out.println(word29);
+        System.out.println(word30);
+        String containsString = "Completely random text in English. " +
+                "In it, we just need to determine how many" +
+                " times the character 'a' occurs there." +
+                " And we can use the split method and the length method.";
+        System.out.println("Количество слов" + " " + arrayOfWords.length);
+      //String[] arrayOfWords = st.split("a", buk.toLowerCase());
+        //String[] arrayOfWords = st.split("a", buk.toUpperCase());
+        //System.out.println(arrayOfWords);
+        //String buk = arrayOfWords['a'];
+    }
+}
