1f2d40f000f8aa43c0ac79d4ac183ad7a4437a17
Victoriiiia <viktoriyagolovataya@gmail.com>
2023-11-07 20:56:10 +0900

    hw6666

--- src/lesson2/Homework_1.java
+++ src/lesson2/Homework_1.java
@@ -4,10 +4,9 @@ public class Homework_1 {
     public static void main(String[] args) {
         int a=7;
         int b=9;
-        a = a+b;
-        b = a-b;
-        a=9;
-        b=7;
+        a=a+b;
+        b=a-b;
+        a=a-b;
         System.out.println("Вывести значение а"+a);
         System.out.println("Вывести значение b"+b);
 
