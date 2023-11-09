2a4cc7056426af9042311cc209a3abd171b4ad86
Victoriiiia <viktoriyagolovataya@gmail.com>
2023-11-09 18:27:10 +0900

    hey 6hwooork

--- src/lesson7/Homework_6.java
+++ src/lesson7/Homework_6.java
@@ -5,14 +5,9 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class Homework_6 {
-    //Пользователь задает размерность двумерного массива с клавиатуры.
-//Массив заполняется случайными числами от 0 до 1000.
-//Необходимо создать одномерный массив, состоящий из максимальных значений
-//каждого отдельного массива входящего в двумерный.
-//Новый полученный массив вывести на экран.
     public static void main(String[] args) {
         int[][] arrayInt = {{123, 87, 234, 31, 92}, {925, 547, 26, 51, 94}};
-        int [] arrayRez = new int[5];
+        int[] arrayRez = new int[5];
         System.out.println(Arrays.deepToString(arrayInt));
         for (int[] ent : arrayInt) {
             System.out.println(Arrays.toString(ent) + " ");
@@ -21,76 +16,88 @@ public class Homework_6 {
                 boolean v = max > ent[i];
                 if (v == false) {
                     max = ent[i];
-                    arrayRez[i]=max;
+                    arrayRez[i] = max;
                 }
-                    System.out.println(Arrays.toString(arrayRez));
-                }
-
+                System.out.println(Arrays.toString(arrayRez));
             }
         }
     }
 
-
-
-
-            //System.out.println("Максимальное число первого массива "+max1);
-
-            //System.out.println("Максимальное число первого массива "+max2);
-
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
-        int targetRow = random.nextInt(5) + 1;
-        int targetCol = random.nextInt(5) + 1;
-        char[][] array = {
-                {'0', '1', '2', '3', '4', '5'},
-                {'1', '-', '-', '-', '-', '-'},
-                {'2', '-', '-', '-', '-', '-'},
-                {'3', '-', '-', '-', '-', '-'},
-                {'4', '-', '-', '-', '-', '-'},
-                {'5', '-', '-', '-', '-', '-'}};
-
-        System.out.println("All Set. Get ready to rumble!");
-        System.out.println(targetRow + ":" + targetCol);
-        System.out.println();
-        while (true) {
-            for (int i = 0; i < array.length; i++)
-                System.out.println(Arrays.toString(array[i]));
-            int shutRow = 0;
-            int shutCol = 0;
-            while (true) {
-                System.out.println("Input Shut Row  1..5 : ");
-                if (scanner.NextInt()) {
-                    shutRow = scanner.nextInt();
-                    boolean b = shutRow > 5;
-                    if (shutRow < 1 || shutRow > 5)
-                    System.out.println("Try again. Not in range 1..5");
-                else break;
-                } else {
-                    System.out.println("Try again. Not an integer.");
-                    scanner.nextLine();
+    public class Aim {
+        public String[][] setAim() {
+            String[][] arrayAim = new String[6][6];
+            for (int i = 0; i < arrayAim.length; i++) {
+                for (int k = 0; k < arrayAim[i].length; k++) {
+                    if (i == 0) {
+                        arrayAim[i][k] = String.valueOf(k);
+                    } else if (k == 0) {
+                        arrayAim[i][k] = String.valueOf(i);
+                    } else {
+                        arrayAim[i][k] = "-";
+                    }
                 }
             }
-            while (true) {
-                System.out.println("Input Shut Column  1..5 : ");
-                if (scanner.hasNextInt()) {
-                    shutCol = scanner.nextInt();
-                    if (shutCol < 1 || shutCol > 5)
-                    System.out.println("Try again. Not in range 1..5");
-        else break;
-                } else {
-                    System.out.println("Try again. Not an integer.");
-                    scanner.nextLine();
-                }
-            }
-            if (shutRow == targetRow && shutCol == targetCol) {
-                System.out.println("YOU HAVE WON !!!");
-                array[targetRow][targetCol] = 'x';
-                break;
-            } else array[shutRow][shutCol] = '*';
+            return arrayAim;
         }
-        System.out.println();
-        for (int i = 0; i < array.length; i++)
-            System.out.println(Arrays.toString(array[i]));
-        System.out.println();
-        System.out.println("Game Over, Bye...");}}
+
+        public void printAim(String[][] aim) {
+            for (String[] strings : aim) {
+                System.out.println(Arrays.toString(strings));
+            }
+        }
+
+        public int[] setAimCoordinates() {
+            int[] aim = {
+                    new Random().nextInt(1, 5),
+                    new Random().nextInt(1, 5)
+            };
+            return aim;
+        }
+
+        public int[] askToShootCoordinates() {
+            Scanner scanner = new Scanner(System.in);
+            int x;
+            do {
+                System.out.println("Insert x between 1 and 5:");
+                x = scanner.nextInt();
+            } while (x < 1 || x > 5);
+
+            int y;
+            do {
+                System.out.println("Insert y between 1 and 5:");
+                y = scanner.nextInt();
+            } while (y < 1 || y > 5);
+            return new int[]{x,y};
+        }
+
+        public void playGame(String[][] field, String name){
+            int[] finalAim = setAimCoordinates();
+            do{
+                int[] aim = askToShootCoordinates();
+                if(Arrays.equals(aim,finalAim)){
+                    field[aim[0]][aim[1]]= "x";
+                    printAim(field);
+                    System.out.println("Congratulation: "+name);
+                    break;
+                } else {
+                    field[aim[0]][aim[1]]= "*";
+                    printAim(field);
+                }
+            }
+            while (true);
+        }
+        public String getName(){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Insert your name:");
+            String name = scanner.nextLine();
+            return name;
+        }
+
+
+        public static void main(String[] args) {
+            Aim aimObject = new Aim();
+            String[][] aimString = new Aim().setAim();
+            aimObject.printAim(aimString);
+            aimObject.playGame(aimString, aimObject.getName());
+        }
+    }
